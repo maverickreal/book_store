4c9122bf9c42b4ad5abb264ae8fde11bc913cfd7
Aditya Saxena <adity09@outlook.com>
2021-10-16 22:22:00 +0530

    towards final(dev/test)

--- app.js
+++ app.js
@@ -13,4 +13,20 @@ app.get('', (req, res) => {
     res.render('index');
 });
 
+app.get('/success', (req,res) => {
+    res.render('success');
+});
+
+app.post('/charge', (req, res) => {
+    const amount = 3000;
+    stripe.customers.create({
+        email: req.body.email,
+        source: req.body.stripeToken
+    }).then(cust => stripe.charges.create({
+        amount, description: 'master the art of mastering mastery',
+        currency: 'inr',
+        customer: cust.id
+    })).then(() => res.render('success'));
+});
+
 app.listen(process.env.PORT || 3000, () => console.log('server'));
